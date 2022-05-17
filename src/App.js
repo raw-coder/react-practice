8f9623228660a7191b11853b9a563141ce79cd20
Young-yun Jeong <yy.jeong@crowdworks.kr>
2022-05-17 13:59:22 +0900

    add App.css

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import './App.css';
 import Hello from "./Hello";
 
 function App() {
+    const name = 'react';
+    const style = {
+        backgroundColor: 'black',
+        color: 'aqua',
+        fontSize: 24,
+        padding: '1rem'
+    }
+
     return (
         <div>
-            <Hello/>
-            <Hello/>
-            <Hello/>
+            {/* comment */}
+            <Hello
+                // inline comment
+            />
+            <div className="gray_box"></div>
+            <div style={style}>{name}</div>
         </div>
     );
 }
