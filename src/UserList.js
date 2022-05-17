d9dda6691645041755097da522ecd79b91984d45
Young-yun Jeong <yy.jeong@crowdworks.kr>
2022-05-17 20:03:01 +0900

    add React.memo()

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from "react";
 
-function User({user, onRemove, onToggle}) {
+const User = React.memo(function({user, onRemove, onToggle}) {
     useEffect(() => {
        console.log('User Component Mounted');
        console.log(user);
@@ -25,7 +25,7 @@ function User({user, onRemove, onToggle}) {
             <button onClick={() => onRemove(user.id)}>Remove</button>
         </div>
     )
-}
+});
 
 function UserList({users, onRemove, onToggle}) {
     return (
@@ -37,4 +37,4 @@ function UserList({users, onRemove, onToggle}) {
     );
 }
 
-export default UserList;
+export default React.memo(UserList);
