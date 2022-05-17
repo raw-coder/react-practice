69232471a7d66db4afb98d860c85b77a607bd225
Young-yun Jeong <yy.jeong@crowdworks.kr>
2022-05-17 17:45:10 +0900

    add CreateUser.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,66 @@
 import './App.css';
-import InputSample from "./InputSample";
 import UserList from "./UserList";
+import {useRef, useState} from "react";
+import CreateUser from "./CreateUser";
 
 function App() {
-    const name = 'react';
-    const style = {
-        backgroundColor: 'black',
-        color: 'aqua',
-        fontSize: 24,
-        padding: '1rem'
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: ''
+    });
+
+    const {username, email} = inputs;
+    const onChange = e => {
+        const {name, value} = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    };
+
+    const [users, setUsers] = useState([
+        {
+            id: 1,
+            username: 'user1',
+            email: 'user1@user.com'
+        },
+        {
+            id: 2,
+            username: 'user2',
+            email: 'user2@user.com'
+        },
+        {
+            id: 3,
+            username: 'user3',
+            email: 'user3@user.com'
+        }
+    ]);
+
+    const nextId = useRef(4);
+    const onCreate = () => {
+        const user = {
+            id: nextId.current,
+            username,
+            email
+        };
+        setUsers([...users, user]);
+        setInputs({
+            username: '',
+            email: ''
+        });
+
+        nextId.current += 1;
     }
 
     return (
         <div>
-            <InputSample/>
-            <UserList/>
+            <CreateUser
+                username={username}
+                email={email}
+                onChange={onChange}
+                onCreate={onCreate}
+            />
+            <UserList users={users}/>
         </div>
 
     );
--- src/UserList.js
+++ src/UserList.js
@@ -8,32 +8,11 @@ function User({user}) {
     )
 }
 
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'user1',
-            email: 'user1@user.com'
-        },
-        {
-            id: 2,
-            username: 'user2',
-            email: 'user2@user.com'
-        },
-        {
-            id: 3,
-            username: 'user3',
-            email: 'user3@user.com'
-        }
-    ];
-
+function UserList({users}) {
     return (
         <div>
             {users.map(user => (
                 <User user={user} key={user.id}/>
-            ))}
-            {users.map((user, index) => (
-                <User user={user} key={index}/>
             ))}
         </div>
     );
